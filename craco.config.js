be00e8614e618bc36e2f2e134411639b27058f4a
Binel Biju <binelbpe@gmail.com>
2024-10-25 13:18:10 +0530

    craco changes

--- craco.config.js
+++ craco.config.js
@@ -5,6 +5,15 @@ module.exports = {
     configure: (webpackConfig) => {
       // Ensure no fallback property is present
       delete webpackConfig.resolve.fallback; // Remove if it exists
+
+      // Example: Add optimization settings
+      webpackConfig.optimization = {
+        splitChunks: {
+          chunks: 'all',
+        },
+        minimize: true,
+      };
+
       return webpackConfig;
     },
   },
